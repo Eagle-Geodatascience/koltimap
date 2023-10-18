76ab5209fda5d501121b176f9fdebf8f8ff9837b
trueearthscience <duossmail@gmail.com>
2023-10-18 16:23:09 +0700

    add shapefile

--- js/main.js
+++ js/main.js
@@ -1,47 +1,42 @@
+
 $(function () {
     // Add basemap collection
-    for (const {icon, caption, url} of BASEMAP_COLLECTION) {
+    for (const { icon, caption, url } of BASEMAP_COLLECTION) {
         appendImageIcon(icon, caption, url);
     }
-})
 
+    // Function to handle shapefile upload
+    $('#add-shapefile').on('click', function () {
+        // Trigger the hidden file input element
+        $('#shapefile-input').click();
+    });
+    $('#shapefile-input').on('change', function (e) {
+        const file = e.target.files[0];
+        if (file) {
+            const reader = new FileReader();
+            reader.onload = function (e) {
+                shp(e.target.result)
+                    .then(geojson => {
+                        map.addSource('shapefile-data', {
+                            type: 'geojson',
+                            data: geojson
+                        });
 
+                        map.addLayer({
+                            id: 'shapefile-layer',
+                            type: 'fill',
+                            source: 'shapefile-data',
+                            paint: {
+                                'fill-color': 'rgba(255, 0, 0, 0.5)'
+                            }
+                        });
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// ! Some Example
-
-// map.on('load', function() {
-//     // Center the map on a specific location
-//     map.flyTo({
-//         center: [120, 0], // New center [lng, lat]
-//         zoom: 8, // Zoom level
-//         speed: 1.5 // Speed of the animation
-//     });
-
-//     // Add a marker to the map
-//     var marker = new mapboxgl.Marker()
-//         .setLngLat([120, 0]) // Marker position [lng, lat]
-//         .addTo(map);
-// });
-
-// map.on('click', function(e) {
-//     // Handle the click event here, e.lngLat contains the clicked coordinates
-//     console.log('Clicked at:', e.lngLat);
-// });
+                        map.fitBounds(turf.bbox(geojson), {
+                            padding: 20
+                        });
+                    });
+            };
+            reader.readAsArrayBuffer(file);
+        }
+    });
+});
